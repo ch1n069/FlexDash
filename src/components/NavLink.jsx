fde2060005e977215f13fe2aac3a326d0ea8699c
Bruno Alpachino <brunolalpachino@gmail.com>
2023-09-09 06:07:28 +0300

    styling of the link element

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NavLink = (item) => {
+const NavLink = ({ item }) => {
+  const linkClass =
+    "flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base";
   return (
-    <div>
-      <Link>
+    <div className="items-center py-2">
+      <Link to={item.path} className={linkClass}>
         {/* this will hold the logo */}
-        <span></span>
-        {item.label}
+        <span className="text-white">{item.icon}</span>
+        <span className="text-white">{item.label}</span>
       </Link>
     </div>
   );
