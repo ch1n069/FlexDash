e608d7f75857de5274fbe07a8c96332b01492b16
Bruno Alpachino <brunolalpachino@gmail.com>
2023-09-09 05:52:40 +0300

    refactoring

--- src/shared/SideBar.jsx
+++ src/shared/SideBar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 // import { IoLogoDesignernews } from "react-icons";
 import { DiCodeigniter } from "react-icons/di";
+import { DASHBOARD_SIDEBAR_LINKS } from "../lib/consts/navigation";
+import NavLink from "../components/NavLink";
 const SideBar = () => {
   return (
     <div className="flex flex-col bg-neutral-900 w-60 p-3">
@@ -8,7 +10,12 @@ const SideBar = () => {
         <DiCodeigniter fontSize={28} color="white" />
         <span className="text-white font-bold">Chinos DashBoard</span>
       </div>
-      <div className="flex-1">Top part</div>
+      <div className="flex-1">
+        {DASHBOARD_SIDEBAR_LINKS.map((item) => {
+          //   <NavLink item={item} />;{item}
+          return <NavLink key={item.key} item={item} />;
+        })}
+      </div>
       <div className="">Bottom part</div>
     </div>
   );
