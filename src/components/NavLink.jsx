654f5d5d374f229cd5034153f0c4c69b319c3c07
Bruno Alpachino <brunolalpachino@gmail.com>
2023-09-09 07:15:23 +0300

    logi to render the active link

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import classNames from "classnames";
 
 const NavLink = ({ item }) => {
   // GET THE current route
@@ -8,7 +9,13 @@ const NavLink = ({ item }) => {
     "flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700  active:bg-neutral-600 rounded-sm text-base";
   return (
     <div className="items-center py-1">
-      <Link to={item.path} className={linkClass}>
+      <Link
+        to={item.path}
+        className={classNames(
+          pathname === item.path ? "bg-yellow-400" : "",
+          linkClass
+        )}
+      >
         {/* this will hold the logo */}
         <span className="text-white">{item.icon}</span>
         <span className="text-white">{item.label}</span>
