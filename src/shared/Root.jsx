c7f5e94a3c1bfe8b3aa617a1e114ca7a90388784
Bruno Alpachino <brunolalpachino@gmail.com>
2023-09-09 05:52:25 +0300

    implemented the logic to render navlinks to the custom link

--- src/shared/Root.jsx
+++ src/shared/Root.jsx
@@ -2,6 +2,8 @@ import React, { Fragment } from "react";
 import { Outlet } from "react-router-dom";
 import DashBoard from "./DashBoard";
 import SideBar from "./SideBar";
+import { DASHBOARD_SIDEBAR_LINKS } from "../lib/consts/navigation";
+import NavLink from "../components/NavLink";
 
 const Root = () => {
   return (
@@ -9,6 +11,7 @@ const Root = () => {
       <Fragment>
         <SideBar />
         {/* <DashBoard /> */}
+
         <Outlet />
       </Fragment>
     </div>
