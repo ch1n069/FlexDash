3b615eded397b0ff678c631c4573a6f6b8ca1877
Bruno Alpachino <brunolalpachino@gmail.com>
2023-09-09 04:11:20 +0300

    tested the routes by creating a

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  const router = createBrowserRouter([{ path: "", element: "" }]);
+  const router = createBrowserRouter([{ path: "", element: <Home /> }]);
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold font-bold hover:bg-red-300 ">
-        Hello world!
-      </h1>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
