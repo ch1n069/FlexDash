cb33156ad72db3dee1bb59a1db5b5c769705a3f1
Bruno Alpachino <brunolalpachino@gmail.com>
2023-09-09 04:09:28 +0300

    created router for the home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { createBrowserRouter } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const router = createBrowserRouter([{ path: "", element: "" }]);
 
   return (
     <>
