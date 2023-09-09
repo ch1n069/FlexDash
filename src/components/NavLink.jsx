350fd5445656b0262af6cee7bc659f75acd69be4
Bruno Alpachino <brunolalpachino@gmail.com>
2023-09-09 07:24:28 +0300

    styling of the active link

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -5,14 +5,16 @@ import classNames from "classnames";
 const NavLink = ({ item }) => {
   // GET THE current route
   const { pathname } = useLocation();
+  //   alert(pathname);
+  console.log(pathname);
   const linkClass =
-    "flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700  active:bg-neutral-600 rounded-sm text-base";
+    "flex items-center gap-2 font-light px-3 py-2 hover:bg-red-400  active:bg-neutral-600 rounded-sm text-base";
   return (
     <div className="items-center py-1">
       <Link
         to={item.path}
         className={classNames(
-          pathname === item.path ? "bg-yellow-400" : "",
+          pathname === item.path ? "bg-red-400" : "text-neutral-400",
           linkClass
         )}
       >
