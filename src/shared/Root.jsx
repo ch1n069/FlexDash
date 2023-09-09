07a0e595e253019e1807c56694a2f413a2c41f1f
Bruno Alpachino <brunolalpachino@gmail.com>
2023-09-09 07:29:59 +0300

    installed headless ui package

--- src/shared/Root.jsx
+++ src/shared/Root.jsx
@@ -4,12 +4,14 @@ import DashBoard from "./DashBoard";
 import SideBar from "./SideBar";
 import { DASHBOARD_SIDEBAR_LINKS } from "../lib/consts/navigation";
 import NavLink from "../components/NavLink";
+import Header from "../components/Header";
 
 const Root = () => {
   return (
     <div className="flex flex-row bg-neutral-100 w-screen h-screen">
       <Fragment>
         <SideBar />
+        <Header />
         {/* <DashBoard /> */}
 
         <Outlet />
