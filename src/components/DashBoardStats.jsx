25abe638d27f0d1f39698fb740768de0f24f9f61
Bruno Alpachino <brunolalpachino@gmail.com>
2023-09-10 05:12:35 +0300

    created dashboard stats component

---
+++ src/components/DashBoardStats.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DashBoardStats = () => {
+  return (
+    <div className="flex gap-4 w-full">
+      <div className="bg-white">a</div>
+      <div className="bg-white">a</div>
+      <div className="bg-white">a</div>
+      <div className="bg-white">a</div>
+    </div>
+  );
+};
+
+export default DashBoardStats;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,9 @@ import React, { Fragment } from "react";
 import { Menu } from "@headlessui/react";
 import { BiSearch } from "react-icons/bi";
 import { BsBell } from "react-icons/bs";
+import { FiSettings } from "react-icons/fi";
+import { FiLogOut } from "react-icons/fi";
+
 import { BsChatLeft } from "react-icons/bs";
 import { Popover, Transition } from "@headlessui/react";
 import { PopoverBody } from "react-bootstrap";
@@ -114,6 +117,52 @@ const Header = () => {
               <span className="sr-only">Bruno</span>
             </div>
           </Menu.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <div className="px-1 py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      className={`${
+                        active ? "bg-red-500 text-white" : "text-gray-900"
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      {active ? (
+                        <FiSettings className="mr-2" size={20} />
+                      ) : (
+                        <FiSettings className="mr-2" aria-hidden="true" />
+                      )}
+                      Account settings
+                    </button>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      className={`${
+                        active ? "bg-red-500 text-white" : "text-gray-900"
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                    >
+                      {active ? (
+                        <FiLogOut className="mr-2" size={20} />
+                      ) : (
+                        <FiLogOut className="mr-2" aria-hidden="true" />
+                      )}
+                      Logout
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
         </Menu>
       </div>
     </div>
