3f2ba35abd4c7a67892e3e1318ecdc2973cc3980
Bruno Alpachino <brunolalpachino@gmail.com>
2023-09-16 03:57:42 +0300

    created firebase config

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDc46EM34nAhUUEuzGruKyvnRdlbIn6C4k",
+  authDomain: "flex-dash.firebaseapp.com",
+  projectId: "flex-dash",
+  storageBucket: "flex-dash.appspot.com",
+  messagingSenderId: "503696292808",
+  appId: "1:503696292808:web:36dd3b918a2ac04200defd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
